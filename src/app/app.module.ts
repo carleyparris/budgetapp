b0832bb761bf1f8e81a6ee72cecca4781f69cc79
Carley Parris <carley.parris@bullhorn.net>
2021-09-15 10:51:02 -0500

    Added Setup Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SetupComponent } from './components/setup/setup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetupComponent
   ],
   imports: [
     BrowserModule
