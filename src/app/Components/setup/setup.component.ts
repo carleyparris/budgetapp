5e72f2bee12db2d1897551cc4f9f071ca5c9f610
carleyparris <carley.parris@bullhorn.com>
2021-09-29 12:45:19 -0500

    Practice component

--- src/app/Components/setup/setup.component.ts
+++ src/app/Components/setup/setup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-setup',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./setup.component.css']
 })
 export class SetupComponent implements OnInit {
-
+welcomeText: string = 'Welcome to MyBudgetApp!'
+testArray: any[] = [1,2,3,4,5]
+displayText: boolean = false
+username: string = ""
   constructor() { }
 
   ngOnInit(): void {
   }
-
+toggleText() {
+this.displayText = !this.displayText
+}
+changeText() {
+  this.username = this.username + "test!?"
+}
 }
